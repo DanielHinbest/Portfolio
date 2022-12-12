5d33bea4d2698e568d212581b5533e3aedbfcceb
daniel hinbest <daniel.hinbest@proton.me>
2022-12-12 13:59:55 -0500

    removed edit_date and is_draft from Post model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-from datetime import date
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
     content = models.TextField(blank=False, null=False)
-    pub_date = models.DateField('date published', blank=False, default=date.today())
-    edit_date = models.DateField('date edited', blank=True, null=True)
+    pub_date = models.DateField('date published', blank=False, default=timezone.now())
     source_code_url = models.URLField(max_length=100, blank=True, null=True)
     website_url = models.URLField(max_length=100, blank=True, null=True)
-    is_draft = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
