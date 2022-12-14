20876e836f36b027af46d51a960e3375ac63a232
daniel hinbest <daniel.hinbest@proton.me>
2022-12-13 21:28:34 -0500

    Formatted post_form.html, created dynamic title

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.forms import ModelForm
 from .models import Post
 
@@ -6,3 +5,8 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'source_code_url', 'website_url']
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
