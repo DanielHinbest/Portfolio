30f39b5a5ea2391488072240c01833550e317581
daniel hinbest <daniel.hinbest@proton.me>
2022-12-16 10:02:31 -0500

    turned off debug

--- danielhinbestportfolio/settings.py
+++ danielhinbestportfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(0%!4#ut0k&-pz%@n#b$4iuz5+-90vzjw=fcciev&d$yu5$9+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
