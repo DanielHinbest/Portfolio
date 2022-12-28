777329a5c7ff41068bfd5e799eea264e7c936a90
daniel hinbest <daniel.hinbest@proton.me>
2022-12-28 01:46:45 -0500

    fixed settings

--- danielhinbestportfolio/settings.py
+++ danielhinbestportfolio/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
+if not DEBUG:
+    STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
 STATICFILES_DIRS = (
     BASE_DIR / "static",
     '/tmp/build_087d35a3/danielhinbestportfolio/static'
