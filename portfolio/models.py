2e868eb9a68bd5cf4a008fbcc4f4a709b844c685
daniel hinbest <daniel.hinbest@proton.me>
2022-12-28 02:57:14 -0500

    updated database field

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=50, blank=False, null=False)
     content = models.TextField(blank=False, null=False)
-    languages = models.CharField(max_length=50, blank=False, null=False)
+    technologies_used = models.CharField(max_length=50, blank=False, null=False)
     pub_date = models.DateField('date published', blank=False, default=timezone.now())
     program_date = models.DateField('date created', blank=False, default=timezone.now())
     source_code_url = models.URLField(max_length=100, blank=True, null=True)
