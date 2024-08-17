2273889202b989b228c33093aa54259f49454e8e
Daniel Hinbest <60987457+DanielHinbest@users.noreply.github.com>
2024-08-17 10:52:42 -0400

    Updated portfolio entires

--- src/components/AboutList.js
+++ src/components/AboutList.js
@@ -5,19 +5,30 @@ class AboutList extends React.Component{
     render() {
         return (
             <div className="row">
-                        <About sectionTitle="My Education" imagePath={process.env.PUBLIC_URL + "/images/education.jpg"} paragraph="I will be graduating from Computer Programming and Analysis at Durham College in June 2022. 
+                        <About sectionTitle="My Education" imagePath={process.env.PUBLIC_URL + "/images/education.jpg"} paragraph="
+                        In 2023, I started studying at Computer Science at Ontario Tech University as part of the college-to-university Advanced Entry program. 
+                As part of this, I received 60 credit hours directly from the transfer. In this program, I have further enhanced my knowledge by taking classes that focus on software quality and testing, mobile development, data science, among others.
+                Now in my final year, I also am eligible to participate in an undergraduate thesis project. As part of this, I am working with Dr. Jeremy Bradbury and Dr. Michael Miljanovic in the Software Engineering & Education Lab (SEER Lab) with a 
+                topic focusing on educating students on software testing.
+                        I be graduated from Computer Programming and Analysis at Durham College in June 2022. 
                 During this time, I have learned about developing desktop, console, and web applications. I have built learned how to design and implement a SQL database, as well as analyze and solve business problems
                 I have received practice by bring provided a case for a business problem and how to plan and implement technology to address it. While attending Durham College, I've also had the opportunity to get involved
-                by participated in activities including the First-Year Student Orientation and Open House as a program student representative and would speak with incoming students to discuss the program's studies
+                by participating in activities including the First-Year Student Orientation and Open House as a program student representative and would speak with incoming students to discuss the program's studies
                 and answer questions about the scope and opportunities. I've also served as a student representative on the Program Advisory Committee, which is a board that meets
                 throughout the year and works to help shape the future of the program with input from students, alumni, faculty, and industry leaders."/>
 
-                        <About sectionTitle="My Work Experience" imagePath={process.env.PUBLIC_URL + "/images/work.png"} paragraph="In August 2022, I started working as an IT Developer at Scotiabank. I primarily
-                        am working in the mainframe systems. From May 2021 to August 2021, I worked in my first position in the IT Industry
-                as an Artificial Intelligence (AI) Research Assistant at the AI Hub at Durham College under the co-op education program. This allowed me to work with AI systems and research how it can be used in our lives.
+                        <About sectionTitle="My Work Experience" imagePath={process.env.PUBLIC_URL + "/images/work.png"} paragraph="
+                Since May 2022, I have been working at the Teching and Learning Centre at Ontario Tech University as Peer Tech Support. In this role, I work with the Educational Technology Analyst to help support students with their use of
+                the educational technologies that are used at Ontario Tech. This includes but is not limited to Canvas, Mentimeter, Kaltura, and Respondus Lockdown Browser.
+                
+                From August 2022-August 2023, I worked at Scotiabank (contracted through Dawn InfoTek) as an IT Developer where I helped maintain the technical infrastructure
+                alongside a team of developers. 
+
+                From May 2021 to August 2021, I worked in my first position in the IT Industry as an Artificial Intelligence (AI) Research Assistant at the AI Hub at Durham College under the co-op education program. This allowed me to work with AI systems and research how it can be used in our lives.
                 I would spend time researching and test technologies that can be used to provide chatbots for companies, and would build software applications for business clients
-                of the AI Hub. From September 2021 to April 2022, I worked part-time as a Peer Tutor at Durham College. During this time, I would meet with
-                students in first and second year, who needed assistance in their courses and would help them gain a further understanding on the concepts in the courses.
+                of the AI Hub. From September 2021 to April 2022, I worked part-time as a Peer Tutor at Durham College. 
+
+                During this time, I would meet with students in first and second year, who needed assistance in their courses and would help them gain a further understanding on the concepts in the courses.
                 In this program, I tutored courses that would focus on C#, C++, Python, Java, and SQL from an introductory to intermediate level."/>
 
                         <About sectionTitle="My Technology Experience" imagePath={process.env.PUBLIC_URL + "/images/coding.png"} paragraph="I have experience building and maintaining applications for web, desktop, mainframe, and mobile environments using many languages and technologies.
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,6 +5,32 @@ class ProjectList extends React.Component {
     render() {
         return (
             <div>
+                {/* <Project 
+                    firstDivClass="col-md-6"
+                    featuretteHeader="Software Quality Assurance Project"
+                    description="The Software Quality Assurance project was developed over the course of a 12 week semester at Ontario Tech University in my second full-time semester.
+                    This was a group project on a team of 4 and the objective was to build a console based game distribution software (similar to Steam) that would allow users to buy and sell games.
+                    This was required to have a C++ front end that would then use a Java or Python backend. The main focus of the project was to demonstrate black box and white box testing in an environment
+                    closer to what would happen in a work environment."
+                    secondDivClass="col-md-6"
+                    image={process.env.PUBLIC_URL + "/images/sqa.png"}
+                    GitHubPath="https://github.com/DanielHinbest/CSCI3310"
+                    imageAltText="A photo of the Groceries To Go cart view"
+                /> */}
+                
+                <Project 
+                    firstDivClass="col-md-6 order-md-2"
+                    featuretteHeader="Software Quality Assurance Project"
+                    description="The Software Quality Assurance project was developed over the course of a 12 week semester at Ontario Tech University in my second full-time semester.
+                    This was a group project on a team of 4 and the objective was to build a console based game distribution software (similar to Steam) that would allow users to buy and sell games.
+                    This was required to have a C++ front end that would then use a Java or Python backend. The main focus of the project was to demonstrate black box and white box testing in an environment
+                    closer to what would happen in a work environment."
+                    secondDivClass="col-md-6"
+                    image={process.env.PUBLIC_URL + "/images/sqa.png"}
+                    GitHubPath="https://github.com/DanielHinbest/csci3060"
+                    imageAltText="A photo of the Software Quality Assurace project"
+                />
+
                 <Project
                     firstDivClass="col-md-6"
                     featuretteHeader="Capstone Project: Groceries To Go"
@@ -20,7 +46,7 @@ class ProjectList extends React.Component {
                 <Project
                     firstDivClass="col-md-6 order-md-2"
                     featuretteHeader="Database Application: McLaughlin University"
-                    description="McLaughlin University was developed in a Database Development class with in a group of four classmates, Ryan, Anna, and Yash, in our fifth semester
+                    description="McLaughlin University was developed in a Database Development class with in a group of four classmates in our fifth semester
                     in Computer Programming and Analysis. We were given a list of business cases and selected McLaughlin University because we felt we had a good plan for building the application.
                     The application was developed using C#, WPF, and uses a SQL Server database. It was designed to allow the donation committee to add donations and view the overall funding
                     for each campus and program, as well as generate a series of reports for the user."
